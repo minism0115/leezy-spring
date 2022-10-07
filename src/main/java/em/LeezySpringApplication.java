ea3f8f52da9e2c8d46c3174e5b3d983fcd181f95
김현철 <foxtori8946@gmail.com>
2022-10-07 10:46:46 +0900

    Initial commit
    - Spring Web`spring-boot-starter-web`
    - Spring Boot DevTools`spring-boot-devtools`
    - Lombok`lombok`
    - Spring Data JPA`spring-boot-starter-data-jpa`
        - H2 Database`com.h2database:h2`
        - MySQL Driver`com.oracle.database.jdbc:ojdbc8`
        - Oracle Driver`mysql:mysql-connector-java`
    - Spring REST Docs`spring-restdocs-mockmvc`

---
+++ src/main/java/em/LeezySpringApplication.java
@@ -0,0 +1,13 @@
+package em;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeezySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeezySpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/em/LeezySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package em;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeezySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
